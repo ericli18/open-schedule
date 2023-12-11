c70b1d2983112c020194fc3f79834b1dc3865988
Eric Li <eric.li.8736@gmail.com>
2023-12-11 13:24:33 -0600

    Add ShadCN UI

--- src/app/calendar/page.jsx
+++ src/app/calendar/page.jsx
@@ -2,18 +2,44 @@
 import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import dayjs from "dayjs";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { useState } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 
 export default function Page({ shifts, employees }) {
   const localizer = dayjsLocalizer(dayjs)
   const [view, setView] = useState('week')
 
+  const clickRef = useRef(null)
+
+  useEffect(() => {
+    /**
+     * What Is This?
+     * This is to prevent a memory leak, in the off chance that you
+     * teardown your interface prior to the timed method being called.
+     */
+    return () => {
+      window.clearTimeout(clickRef?.current)
+    }
+  }, [])
+
+  const onSelectEvent = useCallback((calEvent) => {
+    /**
+     * Here we are waiting 250 milliseconds (use what you want) prior to firing
+     * our method. Why? Because both 'click' and 'doubleClick'
+     * would fire, in the event of a 'doubleClick'. By doing
+     * this, the 'click' handler is overridden by the 'doubleClick'
+     * action.
+     */
+    window.clearTimeout(clickRef?.current)
+    clickRef.current = window.setTimeout(() => {
+      window.alert(JSON.stringify(calEvent, null, 4))
+    }, 250)
+  }, [])
 
   const events = [
     {
-      title: 'Headcount : 2',
+      title: 'Morning \n Headcount : 2',
       start: new Date('2023-12-10T13:45:00-05:00'),
-      end: new Date('2023-12-11T01:00:00-05:00'),
+      end: new Date('2023-12-10T21:00:00-05:00'),
       resourceId: 1,
     }
   ]
@@ -28,7 +54,8 @@ export default function Page({ shifts, employees }) {
         endAccessor="end"
         style={{ minHeight: 500, width: "80%"}}
         views={['week', 'day']}
-        view="week"
+        defaultView="week"
+        onSelectEvent={onSelectEvent}
       />
     </div>
   )
