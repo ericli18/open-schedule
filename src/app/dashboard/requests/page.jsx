e61beecd75d69557e2d3236443f366262a8ca14a
Eric Li <eric.li.8736@gmail.com>
2024-01-09 22:06:45 -0600

    Create conditional component

--- src/app/dashboard/requests/page.jsx
+++ src/app/dashboard/requests/page.jsx
@@ -1,21 +1,21 @@
-import { getEmployees } from "@/lib/data"
+import { getEmployees } from "@/lib/data";
 
 export default async function Page() {
-  const employees = await getEmployees()
+  const employees = await getEmployees();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl text-neutral-900">Welcome to the dashboard!</h1>
-      <p className="text-lg text-neutral-700">This is a protected route.</p>
-      <div className="flex flex-col items-center justify-center">
-        <h2 className="text-2xl text-neutral-900">Employees</h2>
-        <ul className="mt-4">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className='text-4xl text-neutral-900'>Welcome to the dashboard!</h1>
+      <p className='text-lg text-neutral-700'>This is a protected route.</p>
+      <div className='flex flex-col items-center justify-center'>
+        <h2 className='text-2xl text-neutral-900'>Employees</h2>
+        <ul className='mt-4'>
           {employees.map((employee) => (
-            <li key={employee.id} className="text-lg text-neutral-700">
+            <li key={employee.id} className='text-lg text-neutral-700'>
               {employee.name}
             </li>
           ))}
         </ul>
       </div>
     </main>
-  )
+  );
 }
